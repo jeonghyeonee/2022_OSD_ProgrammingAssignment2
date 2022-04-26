755857a43be05a14f0a2b5bcb2fdf70ca688bce7
jeonghyeonee <jlh1218@naver.com>
2022-04-27 02:21:42 +0900

    getPID()가 되는 듯?

--- Prograiming Assignment#2/src/Test.java
+++ Prograiming Assignment#2/src/Test.java
@@ -1,30 +1,77 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Test implements Runnable, PIDManager{
 
     public static long startTime;
     public static int lifetimeProgram;
     public static int lifetimeThread;
+    public static int cur = MIN_PID;  // current pid
+    public static int[] pidArr = new int[MAX_PID - MIN_PID + 1];
+    public static int semaphore = MAX_PID - MIN_PID + 1;
+    public static int select;
 
-    int num;
+    int num;    // thread num
 
-    public Test(int num){
+    public Test(int num) {
         this.num = num;
     }
 
+    private Object obj = new Object();
+
     @Override
     public int getPID() {
-        return 0;
+        // if no pids are available, return -1.
+        if (MIN_PID <= cur & cur <= MAX_PID){
+            pidArr[cur-MIN_PID] = 1;    // change flag to represent it is using
+            cur++;
+            return (cur-1);
+        }
+        else{
+            return -1;
+        }
     }
 
     @Override
     public int getPIDWait() {
+        // if no pids are available, block the calling process until a pid becomes available.
+        synchronized (obj){
+            if (semaphore > 0){
+                semaphore--;
+                for (int i=0; i<MAX_PID-MIN_PID+1; i++){
+                    if(pidArr[i] == 0){
+                        pidArr[i] = 1;
+                        return i + MIN_PID;
+                    }
+                }
+            }
+            else{
+                try{
+                    obj.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            semaphore--;
+            for (int i=0; i<MAX_PID-MIN_PID+1; i++){
+                if(pidArr[i] == 0){
+                    pidArr[i] = 1;
+                    return i + MIN_PID;
+                }
+            }
+
+        }
         return 0;
     }
 
     @Override
     public void releasePID(int pid) {
+        synchronized (obj){
+            pidArr[pid-MIN_PID] = 0;
+            semaphore++;
+            obj.notify();
+        }
 
     }
 
@@ -32,19 +79,31 @@ public class Test implements Runnable, PIDManager{
     public void run() {
         int pid = 0;
 
+        if (select == 1){
+            pid = getPID();
+            if (pid == -1){
+                System.out.println("Thread "+ num + " No pids are available");
+                return;
+            }
+        }
+        else{
+            pid = getPIDWait();
+        }
+
         long finishTime = System.currentTimeMillis();
-        long differ = (long) ((finishTime - startTime)/(1000.0));
+        long differ = (long)((finishTime - startTime)/(1000));
 
         if(differ <= lifetimeProgram){
-            System.out.println(pid + " created at Second");
+            System.out.println(num + " created at Second " + differ + " / " + pid);
         }
 
         if(differ+lifetimeThread <= lifetimeProgram){
-            System.out.println(pid + " destroyed at Second");
+            System.out.println(num + " destroyed at Second " + (differ + lifetimeThread));
         }
         releasePID(pid);
 
     }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -61,8 +120,13 @@ public class Test implements Runnable, PIDManager{
         System.out.print("Please input the lifetime of the thread : ");
         lifetimeThread = sc.nextInt();
 
-        System.out.println("Test program is initialized with " + threadNum + " thread and " + lifetimeProgram + " seconds, with life time "
-                + lifetimeThread +" seconds of each thread");
+        // select (1) getPID() or (2) getPIDWait()
+        System.out.println("------------------------------------------");
+        System.out.println("(1) getPID()\t(2) getPIDWait()");
+        System.out.print("Please select the number you want to use : ");
+        select = sc.nextInt();
+        System.out.println("------------------------------------------");
+        System.out.println("Test program is initialized with " + threadNum + " thread and " + lifetimeProgram + " seconds, with life time " + lifetimeThread +" seconds of each thread");
 
 
         startTime = System.currentTimeMillis();
@@ -71,14 +135,14 @@ public class Test implements Runnable, PIDManager{
 
         for (int i=1; i<=threadNum; i++){
             long Time = System.currentTimeMillis();
-            long differ = (long) ((Time-startTime)/(1000.0));
+            long differ = (long)((Time-startTime)/(1000));
 
             if(differ > lifetimeProgram){
                 break;
             }
             try{
-                Thread.sleep((int) (Math.random() * 1000));
-            } catch (InterruptedException e){
+                Thread.sleep((int)(Math.random() * 1000));
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
@@ -89,7 +153,7 @@ public class Test implements Runnable, PIDManager{
 
         while(true){
             long finishTime = System.currentTimeMillis();
-            long differ = (long)((finishTime - startTime)/(1000.0));
+            long differ = (long)((finishTime - startTime)/(1000));
 
             for(int i=0; i<arr.size(); i++){
                 arr.get(i).interrupt();
@@ -101,13 +165,11 @@ public class Test implements Runnable, PIDManager{
             }
         }
 
-        System.out.println();
-//        System.out.println("check the number of PID using");
-
         System.exit(0);
 
 
 
     }
+
 
 }
